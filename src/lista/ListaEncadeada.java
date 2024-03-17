fe066ae54bbe5bf1bbc2135d2c7e67f7ab27cc40
= <=>
2024-03-17 19:45:19 -0300

    add list length

--- src/lista/ListaEncadeada.java
+++ src/lista/ListaEncadeada.java
@@ -2,11 +2,17 @@ package lista;
 
 public class ListaEncadeada <T>{
     private  No<T> inicio;
+    private int tamanho = 0;
 
     public  void adiciona(T elemento){
         No<T> celula = new No<>(elemento);
         this.inicio = celula;
+        this.tamanho ++;
 
+    }
+
+    public int getTamanho(){
+        return  this.tamanho;
     }
 
     @Override
--- src/lista/teste/ListaEncadeadaTeste.java
+++ src/lista/teste/ListaEncadeadaTeste.java
@@ -6,7 +6,9 @@ public class ListaEncadeadaTeste {
     public static  void main (String[] args){
         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
         lista.adiciona(1);
+        lista.adiciona(3);
 
         System.out.println(lista);
+        System.out.println("Tamanho = "+lista.getTamanho());
     }
 }
